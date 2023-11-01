c3769126c730aa3911a8f074efe1ba22cc0a1871
Terrance <149449194+TerryJG@users.noreply.github.com>
2023-11-01 11:18:50 -0400

    touch func added, messages added

--- key_map.js
+++ key_map.js
@@ -15,7 +15,7 @@ const keyMappings = {
     'Backspace': 'Backspace',
 };
 
-// Event listener for pressed key
+// When key is pressed
 document.addEventListener("keydown", (event) => {
     const keyPressed = event.key;
     const mappedKey = keyMappings[keyPressed];   // reference keyMappings
@@ -41,7 +41,7 @@ document.addEventListener("keydown", (event) => {
             }
         }
 
-        // Add a class to the element when a key is pressed
+        // When key is pressed, add class to element
         if (mappedKey) {
             const element = document.getElementById(keyMappings[keyPressed]);
             if (element && !element.classList.contains("key-input")) {
@@ -51,26 +51,74 @@ document.addEventListener("keydown", (event) => {
     }
 });
 
-// Event listener for pressed key when released
+// When pressed key is released
 document.addEventListener("keyup", (event) => {
     const keyPressed = event.key;
     if (dtmfFrequencies[keyPressed] && dtmf.status === 1) {
         dtmf.stop();
     }
-    // Remove the class from the element when the key is released
+    // when key is released, remove class from element
     const element = document.getElementById(keyMappings[keyPressed]);
     if (element) {
         element.classList.remove("key-input");
     }
 });
 
+// Touchscreen Functions
+//0-9; *&#
 function handleTouch(event) {
     const touchedElement = event.target;
     if (touchedElement.classList.contains("num-letter")) {
         const num = touchedElement.id;
-        document.getElementById("display").textContent += num;
+
+        // Check if input length is below limit
+        if (input.length < 7) {
+            input += num;
+            updateOutput();
+        }
+        // Check if pressed key corresponds to DTMF tone
+        const frequencyPair = dtmfFrequencies[num];
+        if (frequencyPair) {
+            dtmf.freq1 = frequencyPair.f1;
+            dtmf.freq2 = frequencyPair.f2;
+
+            if (dtmf.status === 0) {
+                dtmf.start();
+            }
+        }
+        // When key is pressed, add class to element
+        if (!touchedElement.classList.contains("key-input")) {
+            touchedElement.classList.add("key-input");
+        }
     }
 }
-
 const numpadWrapper = document.querySelector(".numpad-wrapper");
 numpadWrapper.addEventListener("touchstart", handleTouch);
+
+ // "back" & "call"
+function handleFuncButtonTouch(event) {
+    const touchedButton = event.target;
+    if (touchedButton.classList.contains("func-button")) {
+        const buttonId = touchedButton.id;
+
+        if (buttonId === "func-button-id") {
+
+        } else if (buttonId === "back") {
+        }
+
+        if (!touchedButton.classList.contains("button-touched")) {
+            touchedButton.classList.add("button-touched");
+        }
+    }
+}
+const funcButtons = document.querySelectorAll(".func-button");
+funcButtons.forEach((button) => {
+    button.addEventListener("touchstart", handleFuncButtonTouch);
+});
+
+funcButtons.forEach((button) => {
+    button.addEventListener("touchend", (event) => {
+        // Remove the class from the button when the touch ends
+        button.classList.remove("button-touched");
+    });
+})
